12529b338e19d6356691ef9ff401c1f1b4da7d96
Kaspar <kylgorvigajava@gmail.com>
2025-05-23 17:27:18 +0300

    testing form

--- src/form.js
+++ src/form.js
@@ -4,6 +4,7 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
     emailjs.sendForm('service_zf1as5l', 'template_83eam1n', this)
         .then(() => {
             alert('Message sent successfully!');
+            console.log('EmailJS success:', response);
             this.reset(); // clear form
         }, (error) => {
             alert('Failed to send message. Please try again.');
