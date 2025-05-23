470d8d1db818f8a47a64ae39701b2a0c1a0ee274
Kaspar <kylgorvigajava@gmail.com>
2025-05-23 17:30:32 +0300

    testing form #2

--- src/form.js
+++ src/form.js
@@ -10,8 +10,9 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
             alert('Failed to send message. Please try again.');
             console.error('EmailJS error:', error);
         });
+
+    (function () {
+        emailjs.init("0sx78NiymcpZL-Ajw");
+    })();
 });
 
-(function () {
-    emailjs.init("0sx78NiymcpZL-Ajw");
-})();
